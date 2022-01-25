3b84a445ab2a0ac423f2bac36c51e3b31bc2eecb
rafalL7 <rafal.sarnecki@gum.gov.pl>
2022-01-25 15:13:19 +0100

    dalej

--- kolejnytest/src/main/java/User.java
+++ kolejnytest/src/main/java/User.java
@@ -1,2 +1,43 @@
+import java.util.Objects;
+
 public class User {
+    String name;
+    String surName;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surName='" + surName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getName().equals(user.getName()) && getSurName().equals(user.getSurName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getSurName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 }

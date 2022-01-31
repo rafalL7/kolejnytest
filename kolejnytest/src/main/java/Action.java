f12a6c14c0884fb057d6cc6b371efbf03e66b696
rafalL7 <rafal.sarnecki@gum.gov.pl>
2022-01-31 10:49:43 +0100

    new

--- kolejnytest/src/main/java/Action.java
+++ kolejnytest/src/main/java/Action.java
@@ -1,5 +1,9 @@
 public class Action {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        User user = new User();
+        user.setName("Rafał");
+        String name = user.getName();
+        System.out.println(name);
     }
 }

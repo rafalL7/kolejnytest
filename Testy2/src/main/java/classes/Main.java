2489f4478e288f043deb41faaa5231a9cc7883c0
rafalL7 <rafal.sarnecki@gum.gov.pl>
2022-03-16 12:45:52 +0100

    Lombok project5

--- Testy2/src/main/java/classes/Main.java
+++ Testy2/src/main/java/classes/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         User user = new User();
+        user.toString();
+        user.setAge(18);
         user.setName("Aga");
-        System.out.println(user.getName());
+        System.out.println(user.getName()+" "+user.getAge());
         user.toString();
         user.setAge(18);
 

cadba97352243e61687ea417016f3cb9b834a818
rafalL7 <rafal.sarnecki@gum.gov.pl>
2022-03-24 17:04:58 +0100

    Checking object

--- Testy2/src/main/java/classes/Main.java
+++ Testy2/src/main/java/classes/Main.java
@@ -8,6 +8,26 @@ public class Main {
         user.setAge(18);
         user.setName("Aga");
         System.out.println(user.getName()+" "+user.getAge());
+        int itemvalue = 100 + 50;
+        System.out.println(itemvalue);
+        itemvalue+=50;
+        System.out.println(itemvalue);
+        if (itemvalue >=200){
+            System.out.println("Zmienna równa lub mniejsza 200");
+        }
+        String someText = "Great string!";
+        Integer someNumber = 0;
+        checkSomeObject(someNumber);
+        checkSomeObject(someText);
 
+    }
+    static void checkSomeObject(Object object) {
+        if (object instanceof String){
+            String someText = (String) object;
+            System.out.println("Text: "+someText);
+        }
+        else {
+            System.out.println("other type");
+        }
     }
 }
